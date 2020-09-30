d112abfe2c777e5bcd823ca08ffe586200925283
casitadelterror <69657855+casitadelterror@users.noreply.github.com>
2020-09-30 09:15:34 -0600

    Add files via upload

--- REPOSITORY/ADMbot.sh
+++ REPOSITORY/ADMbot.sh
@@ -250,7 +250,7 @@ local bot_retorno="$LINE\n"
 ajuda_fun () {
 local bot_retorno="$LINE\n"
          bot_retorno+="$(fun_trans "Hola Amigo")\n"
-         bot_retorno+="$(fun_trans "Bienvenido al BOT") Mod By @Kalix1\n"
+         bot_retorno+="$(fun_trans "Bienvenido al BOT")\n"
          bot_retorno+="$LINE\n"
          bot_retorno+="$(fun_trans "Lista de Comandos Disponibles")\n"
          bot_retorno+="$LINE\n"
